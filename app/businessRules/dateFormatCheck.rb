e64e1f8776510cf06168591074a4f0f710920c8b
niuniu <niuniu@niuniudeMacBook-Pro.local>
2016-05-05 15:18:15 -0700

    Added regular expression for date format

--- app/businessRules/dateFormatCheck.rb
+++ app/businessRules/dateFormatCheck.rb
@@ -3,12 +3,22 @@ def DateFormatCheck(csvin, params)
   case params[:timeFormat]
   when "hh:mm:ss"
     m = /^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/
-    return checkMatch(csvin,m)
+    return checkMatch(csvin, m)
   when "YYYY-MM-DD"
+    m = /^[0-9]{4}\-((1[0-2])|0([1-9]))\-(([1-2][0-9])|0([1-9])|(3[0-1]))$/
+    return checkMatch(csvin, m)
   when "YYYY-MM"
+    m = /^[0-9]{4}\-((1[0-2])|0([1-9]))$/
+    return checkMatch(csvin, m)
   when "YYYY"
+    m = /^[0-9]{4}$/
+    return checkMatch(csvin, m)
   when "DD/MM/YYYY"
+    m = /^(([1-2][0-9])|0([1-9])|(3[0-1]))\/((1[0-2])|0([1-9]))\/[0-9]{4}$/
+    return checkMatch(csvin, m)
   when "MM/DD/YYYY"
+    m = /^((1[0-2])|0([1-9]))\/(([1-2][0-9])|0([1-9])|(3[0-1]))\/[0-9]{4}$/
+    return checkMatch(csvin, m)
   else #default case
   end
 end
